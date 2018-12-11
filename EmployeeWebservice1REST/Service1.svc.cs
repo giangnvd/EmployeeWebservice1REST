3fe38b9918856d49209c1cd01d6f553b36a7e7bb
Van Giang <giangnvd00570@fpt.edu.vn>
2018-12-11 08:51:01 +0700

    Hoan Thanh

---
+++ EmployeeWebservice1REST/Service1.svc.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace EmployeeWebservice1REST
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        EmployeeDataDataContext data = new EmployeeDataDataContext();
+        public bool AddEmployee(Employee eml)
+        {
+            try
+            {
+                data.Employees.InsertOnSubmit(eml);
+                data.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteEmployee(int idE)
+        {
+            try
+            {
+                Employee employeeToDelete =
+                    (from Employee in data.Employees where Employee.empID == idE select Employee).Single();
+                data.Employees.DeleteOnSubmit(employeeToDelete);
+                data.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        //public CompositeType GetDataUsingDataContract(CompositeType composite)
+        //{
+        //    if (composite == null)
+        //    {
+        //        throw new ArgumentNullException("composite");
+        //    }
+        //    if (composite.BoolValue)
+        //    {
+        //        composite.StringValue += "Suffix";
+        //    }
+        //    return composite;
+        //}
+
+        public List<Employee> GetProductList()
+        {
+            try
+            {
+                return (from Employee in data.Employees select Employee).ToList();
+
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public bool UpdateEmployee(Employee eml)
+        {
+            Employee employeeToModify =
+          (from Employee in data.Employees where Employee.empID == eml.empID select Employee).Single();
+            employeeToModify.age = eml.age;
+            employeeToModify.firstName = eml.address;  
+            employeeToModify.firstName = eml.firstName;
+            employeeToModify.lastName = eml.lastName;
+            data.SubmitChanges();
+            return true;
+        }
+    }
+}
---
+++ UWP/MainPage.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+            void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            getEmployee();
+        }
+        async void getEmployee()
+        {
+            try
+            {
+                ProgressBar.IsIndeterminate = true;
+                ProgressBar.Visibility = Visibility.Visible;
+                GridViewEmployee.ItemsSource = await webService.GetProductListAsync();
+                ProgressBar.Visibility = Visibility.Collapsed;
+                ProgressBar.IsIndeterminate = false;
+             }
+            catch(Exception ex)
+            {
+                MessageDialog messageDialog = new MessageDialog(ex.Message);
+                await messageDialog.ShowAsync();
+                ProgressBar.Visibility = Visibility.Collapsed;
+                ProgressBar.IsIndeterminate = false;
+
+            }
+        }  
+        private async void GridViewEmpolyee_SelectionChanger(object sender , SelectionChangedEventArgs e)
+        {
+            try
+            {
+                if (e.AddedItems.Count != 0)
+                {
+                    Employee selectedEmployee = e.AddedItems[0] as Employee;
+                    TextBoxName.Text = selectedEmployee.firstName;
+                    TextBoxSurName.Text = selectedEmployee.lastName;
+                    TextBoxCity.Text = selectedEmployee.address;
+                    TextBoxAge.Text = selectedEmployee.Age.Totring();   
+
+                }
+            }
+            catch
+            {
+                MessageDialog messageDialog = new MessageDialog("Error Data! ");
+                await messageDialog.ShowAsync();
+                ProgressBar.Visibility = Visibility.Collapsed;
+                ProgressBar.IsIndeterminate = false;    
+            }
+        }
+
+        private  async void ButtonAdd_Click(object sender , RoutedEventArgs e)
+        {
+            try
+            {
+                ProgressBar.IsIndeterminate = true;
+                ProgressBar.Visibility = Visibility.Visible;
+                Employee newEmployee = new Employee();
+                newEmployee.firsName = TextBoxName.Text;
+                newEmployee.lastName = TextBoxSurName.Text;
+                newEmployee.Age = Int32.Parse(TextBoxAge.Text);
+                newEmployee.address = TextBoxCity.Text;
+                bool result = await webService.AddEmployeeAsync(newEmployee);
+                ProgressBar.Visibility = Visibility.Collapsed;
+                ProgressBar.IsIndeterminate = false;
+                if (result== true)
+                {
+                    Me
+
+                }
+            }
+        } 
+
+    }
+}
